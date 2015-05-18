c5fb62ad19aa2300cd149d9ca011b3a4819fd4e0
Ryan Rentfro <rrentfro@gdrgroup.com>
2015-05-17 22:31:34 -0700

    Initial commit

---
+++ 001-addition.php
@@ -0,0 +1,47 @@
+<?php
+/**
+* Math Examples is a collection of math examples to learn basic to advanced math with programming 
+* 
+* @package Math Examples
+* @author Ryan Rentfro
+* @license MIT
+* @link https://github.com/SourceCode
+* @link http://rrentfro.net 
+*/
+
+
+echo '<pre>';
+/*
+* Basic addition example 1
+*/
+echo 'Addition Example #1:' . "\n";
+echo '270 + 485 = ';
+echo 270 + 485;
+echo "\n\n";
+
+/*
+* Basic addition example 2
+*/
+echo 'Addition Example #2:' . "\n";
+echo '98123 + 7278 = ';
+echo 98123 + 7278;
+echo "\n\n";
+
+/*
+* Intermediate addition example 1
+*/
+echo 'Addition Example #3:' . "\n";
+echo '923 + 778 + 12 + 73 + 519 = ';
+echo 923 + 778 + 12 + 73 + 519;
+echo "\n\n";
+
+/*
+* Intermediate addition example 2
+*/
+echo 'Addition Example #4:' . "\n";
+echo '87 + 93 + 12 + 77 + 12 + 63 + 728 = ';
+echo 87 + 93 + 12 + 77 + 12 + 63 + 728;
+echo "\n\n";
+
+echo '</pre>';
+?>
---
+++ 003-multiplication.php
@@ -0,0 +1,47 @@
+<?php
+/**
+* Math Examples is a collection of math examples to learn basic to advanced math with programming 
+* 
+* @package Math Examples
+* @author Ryan Rentfro
+* @license MIT
+* @link https://github.com/SourceCode
+* @link http://rrentfro.net 
+*/
+
+
+echo '<pre>';
+/*
+* Basic multiplication example 1
+*/
+echo 'Multiplication Example #1:' . "\n";
+echo '485 * 270 = ';
+echo 485 * 270;
+echo "\n\n";
+
+/*
+* Basic multiplication example 2
+*/
+echo 'Multiplication Example #2:' . "\n";
+echo '98123 * 7278 = ';
+echo 98123 * 7278;
+echo "\n\n";
+
+/*
+* Intermediate multiplication example 1
+*/
+echo 'Multiplication Example #3:' . "\n";
+echo '923 * 778 * 12 * 73 * 5 = ';
+echo 923 * 778 * 12 * 73 * 5;
+echo "\n\n";
+
+/*
+* Intermediate multiplication example 2
+*/
+echo 'Multiplication Example #4:' . "\n";
+echo '2987 * 93 * 12 * 77 * 12 * 63 * 728 = ';
+echo 2987 * 93 * 12 * 77 * 12 * 63 * 728;
+echo "\n\n";
+
+echo '</pre>';
+?>
