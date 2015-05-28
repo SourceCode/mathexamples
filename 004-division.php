d7baf4ae3a7b01e56f337bb4874159ad023bee05
SourceCode <rrentfro@gmail.com>
2015-05-27 19:15:14 -0700

    Adds division example

---
+++ 004-division.php
@@ -0,0 +1,65 @@
+<?php
+/**
+* Math Examples is a collection of math examples to learn basic to advanced math with programming 
+* 
+* @package Math Examples
+* @author Ryan Rentfro
+* @license MIT
+* @link https://github.com/SourceCode
+* @link http://rrentfro.net 
+*/
+
+
+echo '<pre>';
+/*
+* Basic whole number division
+*/
+echo 'Division Example #1:' . "\n";
+echo '24 / 4 = ';
+echo 24 / 4;
+echo "\n\n";
+
+/*
+* Basic whole number division 2
+*/
+echo 'Division Example #2:' . "\n";
+echo '256 / 8 = ';
+echo 256 / 8;
+echo "\n\n";
+
+/*
+* Basic whole number division 3
+*/
+echo 'Division Example #3:' . "\n";
+echo '256 / 8 / 8 = ';
+echo 256 / 8 / 8;
+echo "\n\n";
+
+/*
+* Basic whole number division 4
+*/
+echo 'Division Example #4:' . "\n";
+echo '256 / 8 / 8 / 2 = ';
+echo 256 / 8 / 8 / 2;
+echo "\n\n";
+
+/*
+* Basic whole number division 5
+*/
+echo 'Division Example #5:' . "\n";
+echo '13 / 2 = ';
+echo floor(13 / 2);
+echo ' remainder: ' . (13 % 2);
+echo "\n\n";
+
+/*
+* Basic whole number division 6
+*/
+echo 'Division Example #5:' . "\n";
+echo '4873 / 3 = ';
+echo floor(4873 / 7);
+echo ' remainder: ' . (4873 % 3);
+echo "\n\n";
+
+echo '</pre>';
+?>
